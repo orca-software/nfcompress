35f122fe02c51857dcc24a907a5d5f2e90ffeb70
Jaap Versteegh <j.r.versteegh@gmail.com>
2017-11-22 17:57:56 +0100

    Remove dependency on nfdump's nffile.h

--- src/types.h
+++ src/types.h
@@ -5,11 +5,71 @@
 
 // Include nffile for header, stat and block header structures
 #include <sys/types.h>
-#include "../nfdump/bin/nffile.h"
 
+// ** Defines and types from nffile.h
+#define IDENTLEN	128
+#define IDENTNONE	"none"
+
+typedef struct file_header_s {
+	uint16_t	magic;				// magic to recognize nfdump file type and endian type
+#define MAGIC 0xA50C
+
+	uint16_t	version;			// version of binary file layout, incl. magic
+#define LAYOUT_VERSION_1	1
+
+	uint32_t	flags;				
+#define NUM_FLAGS		4
+#define FLAG_NOT_COMPRESSED	0x0		// records are not compressed
+#define FLAG_LZO_COMPRESSED	0x1		// records are LZO compressed
+#define FLAG_ANONYMIZED 	0x2		// flow data are anonimized 
+#define FLAG_CATALOG		0x4		// has a file catalog record after stat record
+#define FLAG_BZ2_COMPRESSED 0x8		// records are BZ2 compressed
+// New flags introduced
 #define FLAG_LZ4_COMPRESSED 0x10
 #define FLAG_LZMA_COMPRESSED 0x20
+	uint32_t	NumBlocks;			// number of data blocks in file
+	char		ident[IDENTLEN];	// string identifier for this file
+} file_header_t;
 
+typedef struct stat_record_s {
+	// overall stat
+	uint64_t	numflows;
+	uint64_t	numbytes;
+	uint64_t	numpackets;
+	// flow stat
+	uint64_t	numflows_tcp;
+	uint64_t	numflows_udp;
+	uint64_t	numflows_icmp;
+	uint64_t	numflows_other;
+	// bytes stat
+	uint64_t	numbytes_tcp;
+	uint64_t	numbytes_udp;
+	uint64_t	numbytes_icmp;
+	uint64_t	numbytes_other;
+	// packet stat
+	uint64_t	numpackets_tcp;
+	uint64_t	numpackets_udp;
+	uint64_t	numpackets_icmp;
+	uint64_t	numpackets_other;
+	// time window
+	uint32_t	first_seen;
+	uint32_t	last_seen;
+	uint16_t	msec_first;
+	uint16_t	msec_last;
+	// other
+	uint32_t	sequence_failure;
+} stat_record_t;
+
+typedef struct data_block_header_s {
+	uint32_t	NumRecords;		// number of data records in data block
+	uint32_t	size;			// size of this block in bytes without this header
+	uint16_t	id;				// Block ID == DATA_BLOCK_TYPE_2
+	uint16_t	flags;			// 0 - compatibility
+								// 1 - block uncompressed
+								// 2 - block compressed
+} data_block_header_t;
+
+// *** end of nffile.h defines and types
 
 typedef enum {
   compressed_none, 
